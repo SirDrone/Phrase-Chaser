8fdb290f9a903c277f0c36d83e86799a52f3fc43
Alexander Kirk <alexkirk95@gmail.com>
2020-06-25 06:18:42 -0700

    Added options, also more symbols

--- README.txt
+++ README.txt
@@ -1,5 +1,5 @@
 -Project: Phrase Chaser
--Version: 1.0
+-Version: 1.1
 -Author:  ***REMOVED***
 
 #====================================================================================================================
@@ -8,7 +8,7 @@
 
 	To enable penetration testers and home users to evaluate the security of passphrases on authorized devices.
 
-	This project will allow stated entities to expand a phrase into many possibilities.
+	This project will allow stated entities to expand a passphrase into many possibilities.
 	If one knew a phrase was "password", or "melon", but didn't know the exact variations a user may have employed,
 	this project will expand those simple phrases into many of their possible derivatives.  It will still work
 	on longer phrases, but they will certainly require more time to spin up for, and obviously be harder to crack.
@@ -46,18 +46,39 @@
 	python3 phrase_chaser.py your_simple_phrase_here
 
 	As mentioned, a pass_list.txt will be created, which you can then plug into a number of brute-force programs.
+	You can also evaluate a passphrase's statistics without expanding variations via the --stats option.
+
+#====================================================================================================================
+
+-Changes Since Version 1.0:
+	
+	1.)  Threw in a few more characters to our symbol translator
+
+	2.)  Provided the option to change the default output file (pass_list.txt) via -o <your_file.txt>
+	
+	3.)  Added the option to evaluate the potential size of the output file and # of passwords generated.
+	This -s (or --stats) option will not create an output file, as phrases won't be chased.
+
+	4.)  Alphanumerical (-n) and alphabetical (-a) only translation restrictions are a thing
+	
+	5.)  Hello changelog :)
 
 #====================================================================================================================
 
 -Additional Remarks:
 
-	This is my second publicly released project.
-
-	Right now, it doesn't do many fancy things, though in the next version, I plan to both:
-	-Allow the user to more easily control the name of the output file
-	-Provide a "count" argument for the user to see how many passwords/bytes would be generated before use
+	This project currently allows one to both expand and evaluate a passphrase's many variations, though there are
+	certainly areas we can improve on.  For instance, when evaluating using the --stats option, both password
+	strength and time-to-crack indicators might be nice.  Granted, I'm bearing in mind the expansion of these
+	phrases was the point of this simple project, though it is something I may add in a later version.
 
 	I may even work to improve speed, but again, I'm bearing in mind this is for simple phrase cracking.
+
+	Currently, if you'd like to see more on your passphrase's time-to-crack, I highly recommend considering:
+	https://github.com/dropbox/zxcvbn
+	https://lowe.github.io/tryzxcvbn/
+
+	The Zxcvbn creators put a lot of soul into their work -- I may even integrate it in the future.
 
 	And as before, if you find any bugs or issues, dislike or like this design, please let me know!
 	I can only grow with the feedback provided.  Thank you for your time and for using this program.
--- phrase_chaser.py
+++ phrase_chaser.py
@@ -6,32 +6,82 @@
 #hydra -l admin -P pass_list.txt -s 80 -f 192.168.1.1 http-get /
 
 import sys
+import argparse
 
-if len(sys.argv) != 2:
-    raise IndexError("Exactly one arg required -- the default phrase to chase.  Exiting..")
+#Do some basic setup: Establish args, create a letter-to-symbol map, etc.
+parser = argparse.ArgumentParser(description='Expand a passphrase into its variations')
+parser.add_argument('Passphrase', metavar='Passphrase', type=str,
+                    help='The passphrase you wish to expand, e.g. kentucky')
+restrictive_commands = parser.add_mutually_exclusive_group()
+restrictive_commands.add_argument('-a', '--alpha', action="store_true",
+               help='Refuse mapping alphabetical characters to symbols/numbers (alphabetical only)')
+restrictive_commands.add_argument('-n', '--numerical', action="store_true",
+               help='Refuse mapping alphabetical characters to symbols (alphanumerical only)')
+parser.add_argument('-s', '--stats', action="store_true",
+               help='Gets statistics about a passphrase as opposed to expanding it')
+parser.add_argument('-o', '--outfile', type=str, default="pass_list.txt",
+               help='Changes the generated output file from pass_list.txt to your choosing')
+args = parser.parse_args()
 phrase = sys.argv[1] 
-leet_map = {'O':['0'], 'L':['1', '!'], 'Z':['2'], 'E':['3'], 'A':['4', '@'], 'S':['5', '$'], 'G':['6'], 'T':['7'], 'B':['8']} 
+if args.numerical:
+    leet_map = {'O':['0'], 'I':['1'], 'L':['1'], 'Z':['2'], 'E':['3'], 'A':['4'], 
+            'S':['5'], 'G':['6', '9'], 'T':['7'], 'B':['8']} 
+else:
+    leet_map = {'O':['0'], 'I':['1', '!', '|'], 'L':['1', '!', '|'], 'Z':['2'], 'E':['3'], 'A':['4', '@'], 
+            'S':['5', '$'], 'G':['6', '9'], 'T':['7', '+'], 'B':['8'], 'C':['(', '{'], 'X':['%']} 
+
+#If --stats is used, this will help convert bytes to a more human-readable format
+def get_simple_measurement(number_of_bytes):
+    simple_measure = number_of_bytes
+    if (simple_measure / 1024) > 1:
+        simple_measure /= 1024
+        if (simple_measure / 1024) > 1:
+            simple_measure /= 1024
+            if (simple_measure / 1024) > 1:
+                simple_measure /= 1024
+                if (simple_measure / 1024) > 1:
+                    return "{} TB".format(round(simple_measure / 1024, 1))
+                else:
+                    return "{} GB".format(round(simple_measure, 1))
+            else:
+                return "{} MB".format(round(simple_measure, 1))
+        else:
+            return "{} KB".format(round(simple_measure, 1))
+    else:
+        return simple_measure
 
 #First things first, we need to get all possible cases for each letter in our phrase
 overall_cases = []
 for letter in phrase:
     letter_cases = []
-    if letter.upper() in leet_map.keys():
+    if not args.alpha and letter.upper() in leet_map.keys():
         case_set = [letter.lower(), letter.upper()] + leet_map[letter.upper()]
         overall_cases.append(case_set)
     else:
         overall_cases.append([letter.lower(), letter.upper()])
 
-#We will then proceed to combine each possible letters' cases into all expanded possibilities
-pass_list = []
-def combinator(expression, word, index):
-    for case in overall_cases[index]:
-        if (len(word) - 1)  > index:
-            combinator(expression + case, word, index + 1)
-        else:
-            pass_list.append(expression + case)
-combinator("", phrase, 0)
+#If the user just wanted to know some basic stats for their phrase:
+if args.stats:
+    number_of_passwords = 1
+    for case_set in overall_cases:
+        number_of_passwords *= len(case_set)
+    number_of_bytes = number_of_passwords * (len(phrase) + 1)
+    human_readable_bytes = get_simple_measurement(number_of_bytes)
+    print("Number of passwords to be generated: {}\nNumber of potential bytes for {}: {}"
+            .format(number_of_passwords, args.outfile, human_readable_bytes))
 
-with open("pass_list.txt", 'w') as plist:
-    for password in pass_list:
-        plist.write(password + '\n')
+#Otherwise, we will then proceed to combine each possible letters' cases into all expanded possibilities
+else:
+    pass_list = []
+    def combinator(expression, word, index):
+        for case in overall_cases[index]:
+            if (len(word) - 1)  > index:
+                combinator(expression + case, word, index + 1)
+            else:
+                pass_list.append(expression + case)
+    combinator("", phrase, 0)
+
+    #And output all combinations to our outfile (pass_list.txt by default)
+    with open(args.outfile, 'w') as plist:
+        for password in pass_list:
+            plist.write(password + '\n')
